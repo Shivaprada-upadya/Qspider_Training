7ee2f4b0dfedd4d44e8f7b221ac8b08af8b22f8d
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-25 19:27:54 +0530

    Added 1st week Java Example Programs

---
+++ Java/part1/Concatination.java
@@ -0,0 +1,18 @@
+// 1. Write a Program to Print the below details in a single line summary "Student Shivaprada is 21 year old, scored grade A,Passed : true, with 91.83 marks".
+
+class Concatination
+{
+public static void main(String [] args)
+{
+ String name="Shivaprada";
+ int age=21;
+ char grade='A';
+ boolean result=true;
+ double marks=96.5;
+ 
+ System.out.print("Student1 "+name+" is "+age+" year old,scored grade "+grade+",Passed: "+result+",with "+marks+" marks");
+
+ 
+}
+ 
+}
