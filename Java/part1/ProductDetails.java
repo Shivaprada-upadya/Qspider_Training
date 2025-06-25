80bbd68d92f9ce977d118df8eae953d126d1f8dc
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-25 19:59:17 +0530

    Added the Product Details Program using Concatination

--- Java/part1/ProductDetails.java
+++ Java/part1/ProductDetails.java
@@ -5,19 +5,15 @@
 	Grade of quanlity
 	Available in stck
 */
-
 class ProductDetails
 {
 public static void main(String [] args)
-{
+	{
 	String product_name="Pen";
 	int quantity=5;
 	double no_of_item=55.50;
 	int quality=20;
 	Boolean in_stock=true;
-
 	System.out.print("Product:"+product_name+" | Quantity:"+quantity+" |Price:"+no_of_item+" each | Quantity:"+quantity+" | In stock:"+in_stock);
-
-}
-
+	}
 }
