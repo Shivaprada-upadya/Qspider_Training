399d476b7903d5867407bfa1672350696d8b0343
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-26 21:14:59 +0530

    Updated folder name Relational_Operator

---
+++ Java/part1/Relational_Operator/RelationalOperator.java
@@ -0,0 +1,21 @@
+
+class RelationalOperator
+{
+   public static void main(String [] args)
+   {
+       char a='A';
+       char b='B';
+       System.out.println(a>b);
+       System.out.println(a<b);
+       System.out.println(a==b);
+       System.out.println(a>=b);
+       System.out.println(b<=a);
+       System.out.println(a!=b);
+       System.out.println('A'>'B');
+       System.out.println('A'=='B');
+       System.out.println('A'!='B');
+       System.out.println('A'>='B');
+       System.out.println('A'<='B');
+       System.out.println('A'<'B');
+   }
+}
