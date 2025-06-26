b441d58b79e1f8bd2c2a694fc20e647a49598f99
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-27 00:10:40 +0530

    Simple Logical OR  Operator Program for char type Example

---
+++ Java/part1/Logical_Operator/LogicalOR1.java
@@ -0,0 +1,11 @@
+package Java.part1.Logical_Operator;
+
+public class LogicalOR1 {
+    public static void main(String[] args) {
+        char v1='q';
+        char v2='j';
+        boolean result= v1==v2 || v2<v1;
+        System.out.println(result);  //true
+    }
+    
+}
