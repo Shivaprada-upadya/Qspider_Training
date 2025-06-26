462364045c1ee1a9e20f0451df4f68ec53b24321
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-26 23:59:28 +0530

    Simple Logical Add Operator Program Example

---
+++ Java/part1/Logical_Operator/LogicalAdd.java
@@ -0,0 +1,12 @@
+//  26/06/2025 
+
+class LogicalAdd
+{
+    public static void main(String[] args) {
+        int a=5;
+        int b=30;
+        boolean result=(a<b && b==a);
+        System.out.println(result);   //false
+        System.out.println(10<10 && 5<40 && 30==30);  //false
+    }
+}
