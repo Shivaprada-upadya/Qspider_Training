5e5db90ad394d1d385777770cad2af0f9db32934
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-26 21:16:36 +0530

    Updated Compound_Assignment_Operator

---
+++ Java/part1/Compound_Assignment_Operator/CAssignmentOp.java
@@ -0,0 +1,20 @@
+class CAssignmentOp
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int b=30;
+        int c=50;
+        a+=5;
+        System.out.println(a); //10
+        b-=2;
+        System.out.println(b); //28
+        c*=1;
+        System.out.println(c); //50
+        a%=2;
+        System.out.println(a); //0
+        c/=5;
+        System.out.println(c); //10
+    }
+    
+}
---
+++ Java/part1/Compound_Assignment_Operator/CAssignmentOp1.java
@@ -0,0 +1,8 @@
+public class CAssignmentOp1 {
+    public static void main(String[] args) {
+        char ch='a';
+        ch+=2;
+        System.out.println(ch);   //c  
+    }
+    
+}
