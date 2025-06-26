e98c70001edf9aec466a6f08231078ae173c7691
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-27 00:08:04 +0530

    Simple Logical OR Operator Program Example

---
+++ Java/part1/Logical_Operator/LogicalOR.java
@@ -0,0 +1,13 @@
+// Logical OR Operator
+// 26/06/2025
+
+class LogicalOR
+{
+    public static void main(String[] args) {
+        int a=100;
+        int b=50;
+        int c=3;
+        boolean result= c<=a || b>=3 || c>a;
+        System.out.println(result);  //true
+    }
+}
