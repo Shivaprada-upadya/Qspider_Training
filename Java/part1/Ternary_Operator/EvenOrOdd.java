804221965bc9ffb4921c8a50dbefb20da29189f7
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-27 12:18:11 +0530

    Even or Odd Usinf Ternary Operator

---
+++ Java/part1/Ternary_Operator/EvenOrOdd.java
@@ -0,0 +1,9 @@
+// Write a Program to Check the given number is Even or Odd Using Ternary Operator.
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int num=22;
+        String result=num%2==0?"Even":"Odd";
+        System.out.println(result);
+    }
+}
