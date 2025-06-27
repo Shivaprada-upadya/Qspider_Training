6e589af04d7070b284ecb31a4a8f166984565926
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-27 12:25:54 +0530

    Samllest Number using Ternary Operator

---
+++ Java/part1/Ternary_Operator/SmallestNum.java
@@ -0,0 +1,9 @@
+public class SmallestNum {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int result=num1<num2?num1:num2;
+        System.out.println("Smallest Number is :"+num1);
+    }
+    
+}
