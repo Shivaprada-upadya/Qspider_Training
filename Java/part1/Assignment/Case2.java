ffb9325f4aa819524ccd737686ca8871bd7237f7
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-28 13:04:42 +0530

    Multiple relational operators- Program Example

---
+++ Java/part1/Assignment/Case2.java
@@ -0,0 +1,10 @@
+public class Case2 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=11;
+        boolean result= 10!=10 && a<b ;
+        System.out.println(result);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
