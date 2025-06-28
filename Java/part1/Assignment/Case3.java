677d41ef5da69135462d026dbb0a2c0005b0ec0a
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-28 13:01:15 +0530

    sop(a=b=c=10)- Program

---
+++ Java/part1/Assignment/Case3.java
@@ -0,0 +1,9 @@
+public class Case3 {
+    public static void main(String[] args) {
+        int a,b,c;
+        System.out.println(a=b=c=10);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
