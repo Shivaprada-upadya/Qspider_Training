8dfa1d96a97e9a5de632553eaf5ade426540109a
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-06-28 12:48:34 +0530

    Largest Number using Ternary Operator

---
+++ Java/part1/Assignment/LargestNum.java
@@ -0,0 +1,12 @@
+class LargestNum
+{
+    public static void main(String[] args) {
+        {
+            int n1,n2;
+            n1=-100;
+            n2=-10;
+            int result=n1>n2?n1:n2;
+            System.out.println(result);
+        }
+    }
+}
