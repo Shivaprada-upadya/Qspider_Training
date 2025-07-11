4fbd33f4f3e31ef6ed3f0220101afb12e2041ab6
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-11 12:25:02 +0530

    Added the Solution for UpperCase Alphabets Using For Loop

---
+++ Java/part1/loop_statements/for_loop/UpperCaseAlphabetForLoop.java
@@ -0,0 +1,12 @@
+//2. Write a Program to print the Upper CaseAlphabets using for loop.
+
+public class UpperCaseAlphabetForLoop {
+    public static void main(String[] args) {
+        for(char i='A';i<='Z';i++)
+    {
+        System.out.println(i+" ");
+    }
+        
+    }
+    
+}
