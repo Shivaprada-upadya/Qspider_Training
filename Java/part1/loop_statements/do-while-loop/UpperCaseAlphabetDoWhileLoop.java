5f020ff8228a08a388e2b16e1858a8eba1f577a4
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-11 12:24:29 +0530

    Added the Solution for UpperCase Alphabets Using Do While Loop

---
+++ Java/part1/loop_statements/do-while-loop/UpperCaseAlphabetDoWhileLoop.java
@@ -0,0 +1,14 @@
+//2. Write a Program to print the Upper CaseAlphabets using for loop.
+
+public class UpperCaseAlphabetDoWhileLoop {
+    public static void main(String[] args) {
+        char i='A';
+      do  
+    {
+        System.out.println(i+" ");
+        i++;
+    }
+    while(i<='Z');      
+    }
+    
+}
