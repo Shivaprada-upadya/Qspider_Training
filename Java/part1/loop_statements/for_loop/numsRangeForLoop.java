62bddd20040c4e9d5025aaa9ccfdd2d5eb682889
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-11 11:34:17 +0530

    Added for loop program

---
+++ Java/part1/loop_statements/for_loop/numsRangeForLoop.java
@@ -0,0 +1,14 @@
+//Write a Program to print the numbers in between the range
+import java.util.Scanner;
+public class numsRangeForLoop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Start and End of the Range: ");
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+        for(int i=start;i<=end;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
