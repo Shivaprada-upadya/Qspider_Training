dac682b8a995ccf9cb5d7312c3cc77fa7705013c
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-14 12:24:41 +0530

    Added Continue Staement Example

---
+++ Java/part1/jumping_statements/continue_statement/ContinueStatementEx1.java
@@ -0,0 +1,19 @@
+
+public class ContinueStatementEx1 {
+    public static void main(String[] args) {
+       for(int i=0;i<=100;i++)
+       {
+        if(i==7)
+        {
+            continue;
+        }
+        System.out.println(i);
+       }
+   }
+    
+}
+
+
+
+   
+
