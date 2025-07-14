eb699824224ba3eb13689c14b0b42f22afc222a0
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-14 12:53:52 +0530

    Added the Sum Of User Inputs Values

---
+++ Java/part1/Basic_Programs/SumOfUserInputs.java
@@ -0,0 +1,20 @@
+// 
+
+import java.util.Scanner;
+
+public class SumOfUserInputs {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("How many Numbers to add: ");
+        int n=sc.nextInt();
+        int sum=0;
+        System.out.println("Enter the Value: ");
+        for(int i=0;i<n;i++)
+        {
+            int num=sc.nextInt();
+            sum=sum+num;
+        }
+
+        System.out.println("Final sum is :"+sum);
+    }
+}
