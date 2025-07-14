7bc747dabf56454816fb93abccb835434b0a2438
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-14 21:39:16 +0530

    Added Half the Number Without Using Arithemtic Oprator

---
+++ Java/part1/Basic_Programs/Assignment3/HalfTheNumberWithoutArithmeticOperator.java
@@ -0,0 +1,12 @@
+// Write a Program to Half the Number Without Using Arithmetic Operator.
+
+import java.util.Scanner;
+public class HalfTheNumberWithoutArithmeticOperator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Value to half the number: ");
+        int num=sc.nextInt();
+        int halfNum=num>>1;
+        System.out.println("Half the value of "+num+" is : "+halfNum);
+    }
+}
