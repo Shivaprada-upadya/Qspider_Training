a82950013fe4fd7c4ef1c40bd38b28d1e58ce333
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-15 19:35:08 +0530

    Added solution for Print the Sum of Numbers In between the Range and which is divisible by the given number

--- Java/part1/Basic_Programs/Assignment6/PrintSumOfNUmInBetweenRangeDivisibleByGivenNum.java
+++ Java/part1/Basic_Programs/Assignment6/PrintSumOfNUmInBetweenRangeDivisibleByGivenNum.java
@@ -1,5 +1,20 @@
-package Assignment6;
+// Write a Program to Print the Sum of Numbers in Between the Range which are divisible by the given number.
 
+import java.util.Scanner;
 public class PrintSumOfNUmInBetweenRangeDivisibleByGivenNum {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num=sc.nextInt();
+        System.out.println("Enter the Start and End of the Range: ");
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+        int sum=0;
+        for(int i=start;i<=end;i++)
+        {
+            if(i%num==0)
+                sum+=i;
+        }
+        System.out.println("The sum of range is: "+sum);
+    }
 }
