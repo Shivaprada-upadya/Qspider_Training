56871aaa78fbcd762f6ac0a4ffcc7037b2529c4d
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-15 19:15:27 +0530

    Added Solution for Substract 2 number Without using Arithmetic Operator

--- Java/part1/Basic_Programs/Assignment6/SubstractionWithoutArithmeticOperator.java
+++ Java/part1/Basic_Programs/Assignment6/SubstractionWithoutArithmeticOperator.java
@@ -1,5 +1,17 @@
-package Assignment6;
+// Write a Program to Perform SunStraction Without Arithmetic Operator.
 
+import java.util.Scanner;
 public class SubstractionWithoutArithmeticOperator {
-    
+   public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter two Value of a Number: ");
+       int num1=sc.nextInt();
+       int num2=sc.nextInt();
+       while(num2!=0)
+       {
+        num1--;
+        num2--;
+       }
+       System.out.println("Substraction of given Number is: "+num1);
+   } 
 }
