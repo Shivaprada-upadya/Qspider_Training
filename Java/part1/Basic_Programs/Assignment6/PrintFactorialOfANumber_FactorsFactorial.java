f2790bd24207115df84323594dfd9c30a8b0b2ec
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-15 19:24:00 +0530

    Added solution for Print the Factorial of a number and Also print factor's of Factorial Number

--- Java/part1/Basic_Programs/Assignment6/PrintFactorialOfANumber_FactorsFactorial.java
+++ Java/part1/Basic_Programs/Assignment6/PrintFactorialOfANumber_FactorsFactorial.java
@@ -1,5 +1,26 @@
-package Assignment6;
+// Write a Program to print a factorial of a Number & Also Print Factor's of the Factorial.
+
+import java.util.Scanner;
 
 public class PrintFactorialOfANumber_FactorsFactorial {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=num;i++)
+        {
+            fact*=i;
+        }
+        System.out.println("Factorial of "+num+" is : "+fact);
+        System.out.print("Factor's of "+fact+" is: ");
+        for(int i=1;i<=fact;i++)
+        {
+            if(fact%i==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
 }
