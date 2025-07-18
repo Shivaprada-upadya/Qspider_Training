3ca8d1a6bd73a8641a4a262f38e82cd8c8ef92eb
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-19 00:11:22 +0530

    Added  Palindrome Program

--- Java/part1/Standard_Programs/palindrome.java
+++ Java/part1/Standard_Programs/palindrome.java
@@ -1,9 +1,22 @@
 // Write a Program to check whether given number is a Panlindrome or Not.
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class palindrome {
     public static void main(String[] args) {
-        int num=Scanner.nextInt();
-
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num=sc.nextInt();
+        int rev=0;
+        int temp=num;
+        while(temp>0)
+        {
+            int ld=temp%10;
+            rev=rev*10+ld;
+            temp/=10;
+        }
+        if(num==rev)
+            System.out.println(num+" is Palindrome"); 
+        else
+            System.out.println(num+" is not a palindrome");       
     }
 }
