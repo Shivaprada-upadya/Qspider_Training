bcb8cdb2b4ab4f6961580d818304ca3b43728778
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-19 00:05:22 +0530

    Created a New file

--- Java/part1/Standard_Programs/palindrome.java
+++ Java/part1/Standard_Programs/palindrome.java
@@ -1,7 +1,9 @@
-package Java.part1.Standard_Programs;
+// Write a Program to check whether given number is a Panlindrome or Not.
+import java.util.Scanner;
 
 public class palindrome {
     public static void main(String[] args) {
-        
+        int num=Scanner.nextInt();
+
     }
 }
