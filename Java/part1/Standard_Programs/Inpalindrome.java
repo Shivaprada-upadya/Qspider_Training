a5a52b2aeaf1781794f3aa8577b621030e0d06ef
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-19 11:40:52 +0530

    Added- Palindrome number in between the range

---
+++ Java/part1/Standard_Programs/Inpalindrome.java
@@ -0,0 +1,30 @@
+// Write a Program print the  Panlindrome in the range.
+
+import java.util.Scanner;
+public class Inpalindrome 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Start and end range: ");
+        int start=sc.nextInt();
+        int end=sc.nextInt();      
+        for(int i=start;i<=end;i++)
+        {
+            int rev=0;
+            int temp=i;
+            while(temp>0)
+            {
+                int ld=temp%10;
+                rev=rev*10+ld;
+                temp/=10;
+            }
+            if(rev==i)
+            {
+                System.out.print(i+" "); 
+            }
+       
+    }
+    }
+}
+
