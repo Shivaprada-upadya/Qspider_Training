d5400b3365b30e75aa40df953f5dd6d4474c9f2f
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-22 15:22:16 +0530

    Added- Print Prime NUmbers I  Range Program

---
+++ Java/part1/Standard_Programs/PrintPrimeNumInRange.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class PrintPrimeNumInRange
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a start - end range:  ");
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+        sc.close();
+        int count;
+        for(int i=start;i<=end;i++)
+        {
+            count=0;
+            for(int j=2;j<=i/2;j++)
+            {
+                if(i%j==0)
+                {
+                    count=1;
+                    break;
+                }
+        }
+
+            if(count==0 && i!=1)
+            {
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
