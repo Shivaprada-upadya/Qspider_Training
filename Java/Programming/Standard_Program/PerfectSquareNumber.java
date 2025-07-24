338d65e0172b758c54cadee888e9d4f4505cadb0
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-24 13:29:46 +0530

    Added - Problem Statement for Perfect Square Number

--- Java/Programming/Standard_Program/PerfectSquareNumber.java
+++ Java/Programming/Standard_Program/PerfectSquareNumber.java
@@ -1,22 +1,17 @@
+// Write a Program to check whether the given number is Perfect Square Number or not.
+// Perfect Square Number - If a Number is a square of another Integer than the Number is Perfect Square Number.
+// Ex:- 1,4,9,16,25,36,49,64,81,100.
 import java.util.Scanner;
-
 public class PerfectSquareNumber {
-
     public static boolean SquareNum(int num)
-    {
-        
+    {       
         for(int i=1;i<num;i++)
         {
             if(num==i*i)
                 return true;
         }
-        return false;
-        
-            
+        return false;           
     }
-
-
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
