ce1b2cb56d9871bd66ff5e6d3ccea3bed81ad58b
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-07-26 15:43:28 +0530

    Added - Arm Strong NUmber Program

---
+++ Java/Programming/Standard_Program/ArmStrongNumber.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+    public static int DigitOfCount(int num)
+    {
+        int DigitCount=0;
+        while(num>0)
+        {
+            DigitCount++;
+            num/=10;
+        }
+        return DigitCount;
+    }
+
+    public static int exponential(int base,int power)
+    {
+        int expo=1;
+        for(int i=1;i<=power;i++)
+        {
+            expo=expo*base;
+        }
+        return expo;
+    }
+
+    public static int SumOfDigits(int num)
+    {
+        int CountDigit=DigitOfCount(num);
+        int sum=0;
+        int temp=num;
+        while(num>0)
+        {
+            int ld=num%10;
+            sum=sum+exponential(ld,CountDigit);
+            num/=10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int num=sc.nextInt();
+        
+        int answer=SumOfDigits(num);
+        if(num==answer)
+            System.out.println(num+" is a ArmStrongNumber");
+    }
+
+}
