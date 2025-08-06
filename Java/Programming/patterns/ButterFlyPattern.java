4b8fa68148e2df7f106b5be958ec07cce4125b80
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-06 15:54:13 +0530

    Added - Butter Fly Program Example

--- Java/Programming/patterns/ButterFlyPattern.java
+++ Java/Programming/patterns/ButterFlyPattern.java
@@ -1,3 +1,31 @@
 public class ButterFlyPattern {
-    
+    public static void main(String[] args) {
+        int str1=1,spc=8,str2=1;
+        for(int r=1;r<=9;r++)
+        {
+            for(int i=1;i<=str1;i++)
+                System.out.print("*");
+
+            for(int j=1;j<=spc;j++)
+                System.out.print(" ");
+
+            for(int k=1;k<=str2;k++)
+                System.out.print("*");
+            System.out.println();
+            if(r<5)
+            {
+                str1++;
+                str2++;
+                spc-=2;
+                
+            }
+            else
+            {
+                str1--;
+                str2--;
+                spc+=2;
+                
+            }
+        }
+    }
 }
