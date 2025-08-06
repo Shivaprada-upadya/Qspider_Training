ca34ea487887ab8a3caccbbc9424f6000ae99a42
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-06 15:55:01 +0530

    Added - Arrays inputs using user (Scanner) method

---
+++ Java/part2/Arrays/UserInputInArray.java
@@ -0,0 +1,23 @@
+package Java.part2.Arrays;
+
+import java.util.Scanner;
+
+public class UserInputInArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of an Array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println("Enter the "+(i+1)+" th Elements");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
