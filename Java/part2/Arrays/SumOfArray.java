5b4051680e25a2a1f1f425bcbbd75500f0b26359
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-07 14:26:54 +0530

    Added - sumof the Array Program

---
+++ Java/part2/Arrays/SumOfArray.java
@@ -0,0 +1,24 @@
+package Java.part2.Arrays;
+
+import java.util.Scanner;
+public class SumOfArray{
+ public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of an Array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];   
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println("Enter the "+(i+1)+" th Elements");
+            arr[i]=sc.nextInt();
+            
+        }
+         int sum=0;
+         for(int i=0;i<arr.length;i++)
+         {
+             sum+=arr[i];
+         }
+         System.out.println("Sum of the Elements is : "+sum);
+    }
+
+}
