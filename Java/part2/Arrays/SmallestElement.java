6a50fe7fd70121b512b6eb11d653a4c4783b4d52
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-08 15:26:39 +0530

    Added - Smallest element in an array program

---
+++ Java/part2/Arrays/SmallestElement.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SmallestElement {
+     public static int LinearSearch(int[] arr)
+    {
+        int smallest=0;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[i]<arr[smallest])
+                smallest=i;                  
+        }
+        return arr[smallest];
+
+        /*
+            int smallest=arr[0];
+            for(int i=1;i<arr.length;i++)
+            {
+            if(arr[i]<smallest)
+                smallest=arr[i];                  
+        }
+        return smallest; 
+        */
+        
+    }
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of the array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Array elements: ");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int smallest=LinearSearch(arr);        
+        System.out.println("Smallest element is: "+smallest);
+        
+    }
+}
