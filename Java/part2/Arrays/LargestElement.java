8c9bf4fb82144f64a7b98b67f4aa1422d6e6d9e7
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-11 10:10:39 +0530

    Added - Largest element in the array

--- Java/part2/Arrays/LargestElement.java
+++ Java/part2/Arrays/LargestElement.java
@@ -12,7 +12,7 @@ public class LargestElement {
         return largest;
     }
     public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter size of the array: ");
         int size=sc.nextInt();
         int arr[]=new int[size];
