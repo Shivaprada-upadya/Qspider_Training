db533f98ca2fc03cbb25b98ea6c033c7f8c316ef
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-08-15 22:55:36 +0530

    Added - practice programs

---
+++ Java/practice/p1.java
@@ -0,0 +1,63 @@
+//package Java.practice;
+
+public class p1 {
+    public static void main(String[] args) {
+        // int a=5;
+        // int b=a++ + ++a;
+        // System.out.println(b); //12
+
+        // int a=5;
+        // int b=2;
+        // int result=a*b+(a++ + --b);
+        // System.out.println(result); //16
+
+        // boolean x=true;
+        // boolean y=false;
+        // System.out.println(x && y || !y);
+
+        // byte b=127;
+        // b++;
+        // System.out.println(b);  -128
+
+        // int x=5;
+        // int y=x++ *2 + --x/2 +x--;
+        // System.out.println(y); //17
+
+        // byte a=10;
+        // byte b=20;
+        // byte c=(byte) (a+b);
+        // System.out.println(c);
+
+        int a=10;
+        System.out.println(a += a-= a*= a/=2) ;// 
+        System.out.println(1+2+"3"+4+5*0); //3340
+
+        // int x=10;
+        // int y=x++ + ++x *x-- - --x;
+        // System.out.println(y); //144
+
+
+        // int x=5;
+        // int y=3;
+        // int z=x++ * (y+2) / --x +y;
+        // System.out.println(z); //8
+
+        // int a=10;
+        // int b=a-- + ++a * (a++ + --a);
+        // System.out.println(b); //210
+
+        // int a=21;
+        // int b=-12;
+        // int res=a+++ + ++a + b++ + ++b;
+        // System.out.println(res); //22
+        // System.out.println(res--); //22
+
+        // int x=7;
+        // int y=3;
+        // System.out.println(x%y *x /y);
+
+        
+        
+
+    }
+}
---
+++ Java/practice/p15.java
@@ -0,0 +1,10 @@
+//package Java.practice;
+
+public class p15 {
+    public static void main(String[] args) {
+        System.out.println(10==10.0f);
+		int a=10;
+        System.out.println(a+= a-= a*= a/=2) ;// 
+        System.out.println(1+2+"3"+4+5*0); //3340
+    }
+}
