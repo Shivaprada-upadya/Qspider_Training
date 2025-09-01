1a090e73bba6d8ac03147be52610a594c112ab47
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-09-01 15:30:27 +0530

    Modified sopln statement

--- Java/Programming/String/SubSTringInString.java
+++ Java/Programming/String/SubSTringInString.java
@@ -4,7 +4,9 @@ public class SubSTringInString {
     public static void main(String []args)
     {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String:");
         String str=new String(sc.nextLine());
+        System.out.print("Enter the Sub string to check:");
         String subStr=new String(sc.nextLine());
         boolean isPresent=false;
         for(int i=0;i<str.length();i++)
