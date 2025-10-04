8456ddc65ff3e222935201849febdea65ea9f1ea
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-10-04 10:16:51 +0530

    two pointer techinque used

--- Web-Devolopment/JavaScript/functions/palindrome.js
+++ Web-Devolopment/JavaScript/functions/palindrome.js
@@ -1,10 +1,33 @@
+//Inbuilt function
 let str = (str)=>{
     let rev =  str.split('').reverse().join('')
     console.log(rev)
-    if(str === rev)
+    if(str == rev)
         return 'palindrome'
     else    
         return 'not a palidrome'
 }
-let res=str('shivprada')
+
+// two pointer technique
+let method = (str)=>{
+   let left=0 ,right =str.length
+   while(left<right)
+   {
+    if(str.charAt(left)==str.charAt(right))
+    {
+        left++;
+        right--;
+    }
+    else{
+        return false;
+    }
+   }
+   return true;
+}
+
+
+
+let res=str('madam')
 console.log(res)
+let ans=method('shiva')
+console.log(ans)
