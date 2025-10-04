8ac60aae773197d2ad67532b798c126cb0540ae2
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-10-04 10:15:58 +0530

    Deleted unwanted files

--- ReactJs/myreactapp/myviteapp/src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-let Footer =()=>{
-    return <h1>Hello i am Footer</h1>
-}
-
-export default Footer
--- ReactJs/myreactapp/myviteapp/src/components/Navbar.jsx
+++
@@ -1,5 +0,0 @@
-let Navbar=()=> {
-    return <h1>Hello IM Navbar</h1>
-}
-
-export default Navbar
