fa3e7ff9f004f7e8d86d4fe3d502ecf960692695
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-10-14 09:42:44 +0530

    updated handleSubmit

--- ReactJs/useState/src/components/Form.jsx
+++ ReactJs/useState/src/components/Form.jsx
@@ -2,14 +2,20 @@ import React,{useState} from "react";
 const Form =()=>{
 
         let [state, setState]= useState("")
+        let [newState, setNewState]= useState("")
 
         let handleChange=(e)=>{
             setState(e.target.value)
         }
 
+        let handleSubmit=(e)=>{
+            e.preventDefault()
+            setNewState(state)
+        }
+
     return(
         <>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <input 
             
             className="bg-info p-2 m-2 rounded"
@@ -25,6 +31,7 @@ const Form =()=>{
             </button>
             <p>Entered Data: {state}</p>
 
+            <p>Submitted Data: {newState}</p>
             
         </form>
 
