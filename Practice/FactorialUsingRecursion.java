54dc5b55b999c2d34fa512e6fcb5a8fe07268521
Shivaprada Upadya <shivaprada2209@gmail.com>
2025-10-16 18:08:48 +0530

    Adde - Factorial Of a Number using Recursion - program

---
+++ Practice/FactorialUsingRecursion.java
@@ -0,0 +1,16 @@
+
+public class FactorialUsingRecursion{
+public static int factorial(int num)
+{
+    if(num<=1)
+        return 1;
+    return num*factorial(num-1);
+}
+
+
+    public static void main(String[] args)
+    {
+        int num=5;
+        System.out.print("Factorial Of "+num+" is : "+factorial(num));
+    }
+}
